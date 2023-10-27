6519fed13b8f0be47da66c2e9831f0721c09e29a
j-enny <jimohn97@gmail.com>
2023-10-27 21:12:16 +0100

    Change Field 'status' & 'bugtype' to choices field

--- wikimedia/bug/models.py
+++ wikimedia/bug/models.py
@@ -6,20 +6,20 @@ class Bug(models.Model):
     
     bug_type= (
         ('error','Error'),
-                 ('new','New Feature'),
-                 ('update','Update Needed')
+        ('new','New Feature'),
+        ('update','Update Needed')
     )
     
     status = (('to do','To Do'),
               ('in progress','In Progress'),
               ('done','Done')
-              )
+    )
     
     
-    description = models.CharField(name="Description", max_length=200)
-    bug_type = models.CharField(name="Type", max_length=50, choices=bug_type)
-    report_date = models.DateTimeField(name="Date Registered")
-    status = models.CharField(name="Status", max_length=20, choices=status)
+    description = models.CharField(max_length=200)
+    bug_type = models.CharField(max_length=20, choices=bug_type)
+    report_date = models.DateTimeField()
+    status = models.CharField(max_length=25, choices=status)
     
     def __str__(self):
         return self.description
